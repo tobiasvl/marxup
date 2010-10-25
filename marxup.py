3695d6e560135c84298f677e9eedf972923990f2
Tobias V. Langhoff <tobiasvl@ifi.uio.no>
2010-10-25 14:02:25 +0200

    begynne på marxup ...

--- marxup.py
+++ marxup.py
@@ -7,31 +7,51 @@ class Tiki:
         self.raw = raw
         self.context = context
 
+    def element(tag, text, options={}):
+        if 'phrase' in options:
+            del options['phrase']
+            text = phrase(text)
+        if 'break' in options:
+            del options['break']
+            text = '<br>\n'.join(text.split('\n'))
+        attributes = ' '.join(['%s=%s' % (k,v) for (k,v) in options.iteritems()])
+        if text:
+            return '<%s%s>%s</%s>' % (tag, attributes, text.strip(), tag)
+        else:
+            return '<%s%s>' % (tag, attributes)
+
     def as_html():
         pass
+
+    def phrase(text):
+        return handle('phrases', text)
 
     def handle(context, text):
         pass
 
-    def element(tag, text, phrased):
-        pass
-
-    def inline_element(tag, text, phrased):
-        pass
+class Cookbook
+    cleaners = {}
+    chunks = []
+    patterns = {}
+    phrases = []
 
     def pattern(context):
-        pass
+        if 'context' in self.patterns:
+            return self.patterns['context']
+        else:
+            self.patterns['context'] = self.compile(context)
 
-    def rule(context, label, pattern, priority):
+    def rule(context, label, pattern, priority=1, method):
         pass
 
     def clean(token, replacement):
-        pass
-
-    def pre_clean(token, replacement):
-        pass
+        self.cleaners[token] = replacement
 
 class Marxup(Tiki):
+    version = '0.6.5'
+
+    brackets = '\([^\s()<>]+|(\([^\s()<>]+\)))*\)'
+
     def __init__():
         pre_clean("&", "&amp;")
         clean("<", "&lt;")
