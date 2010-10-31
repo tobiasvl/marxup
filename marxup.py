3223d5c67d6a59d687fb87bb30a490e8a5679078
Tobias V. Langhoff <tobiasvl@ifi.uio.no>
2010-10-31 22:45:38 +0100

    rydd opp, kommenter

--- marxup.py
+++ marxup.py
@@ -1,16 +1,44 @@
 import re
 
-class Tiki:
+class Cookbook(object):
+    def __init__(self):
+        self.cleaners = {}
+        self.chunks = []
+        self.patterns = {}
+        self.phrases = []
+
+    def chunk(self, block, *args):
+        return self.rule('chunks', block, *args)
+
+    def phrase(self, block, *args):
+        return self.rule('phrases', block, *args)
+
+    def pattern(self, context):
+        if 'context' in self.patterns:
+            return self.patterns['context']
+        else:
+            self.patterns['context'] = self.compile(context) #TODO compile?
+
+    def rule(self, context, label, pattern, method, priority=1):
+        pass
+
+    def clean(self, token, replacement):
+        self.cleaners[token] = replacement
+
+    def __compile(self, raw): #TODO
+        pass
+
+class Tiki(Cookbook):
     groups = ['text', 'meta']
 
-    def __init__(raw, context = None):
-        self.raw = raw
-        self.context = context
+    def __init__(self, raw, context = None):
+        super(Tiki, self).__init__()
+        self.raw, self.context = raw, context
 
-    def element(tag, text, options={}):
+    def element(self, tag, text, options={}):
         if 'phrase' in options:
             del options['phrase']
-            text = phrase(text)
+            text = self.phrase(text)
         if 'break' in options:
             del options['break']
             text = '<br>\n'.join(text.split('\n'))
@@ -20,41 +48,29 @@ class Tiki:
         else:
             return '<%s%s>' % (tag, attributes)
 
-    def as_html():
+    def as_html(self):
+        text = self.raw
+        for (k,v) in self.cleaners.iteritems():
+            text = text.replace(k, v)
+        return handle('chunks', text)
+
+    def phrase(self, text):
+        return self.handle('phrases', text)
+
+    def handle(self, context, text):
         pass
-
-    def phrase(text):
-        return handle('phrases', text)
-
-    def handle(context, text):
-        pass
-
-class Cookbook
-    cleaners = {}
-    chunks = []
-    patterns = {}
-    phrases = []
-
-    def pattern(context):
-        if 'context' in self.patterns:
-            return self.patterns['context']
-        else:
-            self.patterns['context'] = self.compile(context)
-
-    def rule(context, label, pattern, priority=1, method):
-        pass
-
-    def clean(token, replacement):
-        self.cleaners[token] = replacement
 
 class Marxup(Tiki):
     version = '0.6.5'
 
     brackets = '\([^\s()<>]+|(\([^\s()<>]+\)))*\)'
 
-    def __init__():
-        pre_clean("&", "&amp;")
-        clean("<", "&lt;")
-        clean("<", "&gt;")
-
-        #rules go here
+    def __init__(self, raw):
+        super(Marxup, self).__init__(raw)
+        self.clean("&", "&amp;")
+        self.clean("<", "&lt;")
+        self.clean("<", "&gt;")
+        self.clean("(\r\n|\r)", "\n")
+        
+        text = self.chunk('header', '^\=\s*(?<\\1>.+?)$', lambda text: self.element('h3', text))
+        element('h3', text)
